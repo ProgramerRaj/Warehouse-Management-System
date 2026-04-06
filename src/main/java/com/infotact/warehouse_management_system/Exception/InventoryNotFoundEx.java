90f5e046618820340f447b4101e32b5dce24257d
ProgramerRaj <rajpatel250704@gmail.com>
2026-04-06 16:55:17 +0530

    create new custome exception for inventory not found and handle it with global exception handler

---
+++ src/main/java/com/infotact/warehouse_management_system/Exception/InventoryNotFoundEx.java
@@ -0,0 +1,7 @@
+package com.infotact.warehouse_management_system.Exception;
+
+public class InventoryNotFoundEx extends RuntimeException {
+    public InventoryNotFoundEx(String message) {
+        super(message);
+    }
+}
