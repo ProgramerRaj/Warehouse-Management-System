d85c356698fcc11606d6c883612c07d25311efb3
ProgramerRaj <rajpatel250704@gmail.com>
2026-04-08 12:13:12 +0530

    create custome exception---> when order not found in database

---
+++ src/main/java/com/infotact/warehouse_management_system/Exception/OrderNotFoundEx.java
@@ -0,0 +1,7 @@
+package com.infotact.warehouse_management_system.Exception;
+
+public class OrderNotFoundEx extends RuntimeException {
+    public OrderNotFoundEx(String message) {
+        super(message);
+    }
+}
