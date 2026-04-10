7ab55a9ace6011c043c15f5761067966941d9d89
ProgramerRaj <rajpatel250704@gmail.com>
2026-04-10 21:42:46 +0530

    Create one more methode in OrderItemRepo repository for find OrderItem with Order Id and Product Id

--- src/main/java/com/infotact/warehouse_management_system/Repository/OrderItemRepo.java
+++ src/main/java/com/infotact/warehouse_management_system/Repository/OrderItemRepo.java
@@ -4,6 +4,10 @@ import com.infotact.warehouse_management_system.Model.OrderItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface OrderItemRepo extends JpaRepository<OrderItem,Long> {
+
+    Optional<OrderItem> findByOrderIdAndProductId(long orderId, long productId);
 }
