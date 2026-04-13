291e6a2855c8caa8a9ef6a2aa9603477304342ff
ProgramerRaj <rajpatel250704@gmail.com>
2026-04-13 08:59:18 +0530

    Modify getProduct methode and replace 3 parameters to find product in ProductService, create local methods barcodeGenerate, barcodeImageGenerate, qrCodeImageGenerate and Update some code in receiveProductQuantity methode and handleExistingProduct methode

--- src/main/java/com/infotact/warehouse_management_system/Service/ProductService.java
+++ src/main/java/com/infotact/warehouse_management_system/Service/ProductService.java
@@ -1,10 +1,15 @@
 package com.infotact.warehouse_management_system.Service;
 
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.WriterException;
+import com.google.zxing.client.j2se.MatrixToImageWriter;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.oned.Code128Writer;
+import com.google.zxing.qrcode.QRCodeWriter;
 import com.infotact.warehouse_management_system.DTO.Request.ProductAddReq;
 import com.infotact.warehouse_management_system.DTO.Request.ProductReceiveReq;
 import com.infotact.warehouse_management_system.DTO.Response.ProResponse;
 import com.infotact.warehouse_management_system.Enum.ZoneType;
-import com.infotact.warehouse_management_system.Exception.InventoryNotFoundEx;
 import com.infotact.warehouse_management_system.Exception.ProductNotFoundEx;
 import com.infotact.warehouse_management_system.Exception.WarehouseNotFoundEx;
 import com.infotact.warehouse_management_system.Model.*;
@@ -13,6 +18,8 @@ import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.util.List;
 import java.util.Optional;
 
@@ -66,17 +73,42 @@ public class ProductService {
     }
     // get product by id
     @Transactional
-    public ProResponse getProduct(long id){
-        Product p = productRepo.findById(id).
-                orElseThrow(()-> new ProductNotFoundEx("Product not found with id: "+id));
+    public ProResponse getProduct(Long id,
+                                  String barcode,
+                                  String sku){
+
+        Product product = null;
+
+        // Priority-based search
+        if(id != null){
+            product = productRepo.findById(id)
+                    .orElseThrow(() -> new RuntimeException("Product not found by id"));
+        }
+        else if(barcode != null && !barcode.isBlank()){
+            product = productRepo.findByBarcode(barcode)
+                    .orElseThrow(() -> new RuntimeException("Product not found by barcode"));
+        }
+        else if(sku != null && !sku.isBlank()){
+            product = productRepo.findBySku(sku)
+                    .orElseThrow(() -> new RuntimeException("Product not found by sku"));
+        }
+        else{
+            throw new RuntimeException("Provide at least one parameter (id/barcode/sku)");
+        }
 
         // Set product response
         ProResponse response = new ProResponse(
-                p.getId(), p.getName(),
-                p.getMrp(), p.getDiscount(),
-                p.getSellingPrice(), p.getDescription(),
-                p.getSku(),
-                p.getCategory(), p.isActive(),p.getWarehouse().getId()
+                product.getId(),
+                product.getName(),
+                product.getMrp(),
+                product.getDiscount(),
+                product.getSellingPrice(),
+                product.getDescription(),
+                product.getSku(),
+                product.getBarcode(),
+                product.getCategory(),
+                product.isActive(),
+                product.getWarehouse().getId()
         );
         return response;
     }
@@ -94,43 +126,49 @@ public class ProductService {
         List<Inventory> inventories = inventoryRepo.findByProductId(product.getId());
         Long warehouseId = req.getWarehouseId();
 
-        for(Inventory inventory : inventories) {
+        int remaining = req.getQuantity();
+
+        for (Inventory inventory : inventories) {
 
             StorageBin bin = inventory.getBin();
             Long binWarehouseId = bin.getAisle().getZone().getWarehouse().getId();
 
-                if (!binWarehouseId.equals(warehouseId)) {
-                    continue;
-                }
+            if (!binWarehouseId.equals(warehouseId)) {
+                continue;
+            }
 
             int availableSpace = bin.getMaxCapacity() - bin.getUsedCapacity();
 
-            if (availableSpace >= req.getQuantity()) {
+            if (availableSpace <= 0) {
+                continue;
+            }
 
-                // update bin capacity and inventory
-                bin.setUsedCapacity(bin.getUsedCapacity() + req.getQuantity());
-                inventory.setQuantity(inventory.getQuantity() + req.getQuantity());
+            int toAdd = Math.min(availableSpace, remaining);
 
-                binRepo.save(bin);
-                inventoryRepo.save(inventory);
+            inventory.setQuantity(inventory.getQuantity() + toAdd);
+            bin.setUsedCapacity(bin.getUsedCapacity() + toAdd);
 
-                // response
-                return "Product quantity successfully received with id: " + req.getProId();
+            remaining -= toAdd;
+
+            if (remaining == 0) {
+                break;
             }
         }
-        // No space -> find new bin
-        StorageBin newBin = findAvailableBin(
-                warehouseId,
-                product.getCategory().getZoneType(),
-                req.getQuantity()
-        );
 
-        createInventory(product, newBin, req.getQuantity());
+        if (remaining > 0) {
+            StorageBin newBin = findAvailableBin(
+                    warehouseId,
+                    product.getCategory().getZoneType(),
+                    remaining
+            );
+            createInventory(product, newBin, remaining);
+        }
 
         // response
         return "Product quantity successfully received with id: " + req.getProId();
     }
-    // Local methods
+
+    // Local method (Generate sku)
     private String generateSKU(String proCategory,String proName){
         String catCode = proCategory.substring(0,3).toUpperCase();
         String proCode = proName.substring(0,3).toUpperCase();
@@ -142,42 +180,56 @@ public class ProductService {
 
         return sku;
     }
+
+    // Local method (generate barcode)
+    private String generateBarcode(String sku){
+        return "BC-" + sku + "-" + System.currentTimeMillis();
+    }
+
     private ProResponse handleExistingProduct(Product product,
                                               Long warehouseId,
                                               ProductAddReq request) {
 
         List<Inventory> inventoryList = inventoryRepo.findByProductId(product.getId());
 
+        int remaining = request.getQuantity();
+
         for (Inventory inv : inventoryList) {
 
             StorageBin bin = inv.getBin();
             Long binWarehouseId = bin.getAisle().getZone().getWarehouse().getId();
-            {
-            if (!binWarehouseId.equals(warehouseId))
+
+            if (!binWarehouseId.equals(warehouseId)) {
                 continue;
             }
+
             int availableSpace = bin.getMaxCapacity() - bin.getUsedCapacity();
 
-            if (availableSpace >= request.getQuantity()) {
+            if (availableSpace <= 0) {
+                continue;
+            }
 
-                inv.setQuantity(inv.getQuantity() + request.getQuantity());
-                bin.setUsedCapacity(bin.getUsedCapacity() + request.getQuantity());
+            int toAdd = Math.min(availableSpace, remaining);
 
-                binRepo.save(bin);
-                inventoryRepo.save(inv);
+            inv.setQuantity(inv.getQuantity() + toAdd);
+            bin.setUsedCapacity(bin.getUsedCapacity() + toAdd);
 
-                return buildResponse(product);
+            remaining -= toAdd;
+
+            if (remaining == 0){
+                break;
             }
         }
 
-        // No space -> find new bin
-        StorageBin newBin = findAvailableBin(
-                warehouseId,
-                request.getCategory().getZoneType(),
-                request.getQuantity()
-        );
-
-        createInventory(product, newBin, request.getQuantity());
+        // If still remaining → new bin
+        if (remaining > 0) {
+            StorageBin newBin = findAvailableBin(
+                    warehouseId,
+                    request.getCategory().getZoneType(),
+                    remaining
+            );
+            createInventory(product, newBin, remaining);
+        }
 
         return buildResponse(product);
     }
@@ -204,6 +256,8 @@ public class ProductService {
 
         throw new RuntimeException("No space available in this zone");
     }
+
+    // Local method
     private void createInventory(Product product,
                                  StorageBin bin,
                                  Integer quantity) {
@@ -218,12 +272,19 @@ public class ProductService {
         binRepo.save(bin);
         inventoryRepo.save(inventory);
     }
+
+    // Local methode
     private Product createProduct(ProductAddReq request, Warehouse warehouse) {
 
         String sku;
         do {
             sku = generateSKU(request.getCategory().name(), request.getName());
         } while (productRepo.existsBySku(sku));
+
+        String barcode;
+        do {
+            barcode = generateBarcode(sku);
+        } while (productRepo.existsByBarcode(barcode));
 
         Product product = new Product();
         product.setActive(request.getActive());
@@ -232,7 +293,12 @@ public class ProductService {
         product.setCategory(request.getCategory());
         product.setDiscount(request.getDiscount());
         product.setDescription(request.getDescription());
+
         product.setSku(sku);
+        product.setBarcode(barcode);
+        product.setBarcodeImage(generateBarcodeImage(barcode));
+        product.setQrCodeImage(generateQrCodeImage(barcode));
+
         product.setWarehouse(warehouse);
 
         double discRate = (request.getMrp() * request.getDiscount()) / 100;
@@ -240,6 +306,8 @@ public class ProductService {
 
         return productRepo.save(product);
     }
+
+    // Local method
     private ProResponse buildResponse(Product product) {
         return new ProResponse(
                 product.getId(),
@@ -249,9 +317,93 @@ public class ProductService {
                 product.getSellingPrice(),
                 product.getDescription(),
                 product.getSku(),
+                product.getBarcode(),
                 product.getCategory(),
                 product.isActive(),
                 product.getWarehouse().getId()
         );
+    }
+
+    // Local method
+    private byte[] generateBarcodeImage(String barcode){
+
+        if (barcode == null || barcode.isBlank()) {
+            throw new IllegalArgumentException("Barcode cannot be null or empty");
+        }
+        try {
+            Code128Writer barcodeWriter = new Code128Writer();
+
+            BitMatrix bitMatrix = barcodeWriter.encode(
+                    barcode,
+                    BarcodeFormat.CODE_128,
+                    300,
+                    100
+            );
+
+            ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+
+            MatrixToImageWriter.writeToStream(bitMatrix, "PNG", outputStream);
+
+            return outputStream.toByteArray();
+
+        } catch (IOException e) {
+            throw new RuntimeException("Error while writing barcode image", e);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    // Local method
+    private byte[] generateQrCodeImage(String barcode){
+        if (barcode == null || barcode.isBlank()) {
+            throw new IllegalArgumentException("QR text cannot be null or empty");
+        }
+
+        try {
+            QRCodeWriter qrCodeWriter = new QRCodeWriter();
+
+            BitMatrix bitMatrix = qrCodeWriter.encode(
+                    barcode,
+                    BarcodeFormat.QR_CODE,
+                    300,
+                    300
+            );
+
+            ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+
+            MatrixToImageWriter.writeToStream(bitMatrix, "PNG", outputStream);
+
+            return outputStream.toByteArray();
+
+        } catch (WriterException e) {
+            throw new RuntimeException("Error while encoding QR Code", e);
+        } catch (IOException e) {
+            throw new RuntimeException("Error while writing QR image", e);
+        }
+    }
+
+    @Transactional
+    public byte[] getBarcodeImage(Long productId){
+        Product product = productRepo.findById(productId)
+                .orElseThrow(()-> new ProductNotFoundEx("Product not found with id: "+productId));
+
+        byte[] barcodeImage = product.getBarcodeImage();
+
+        if(barcodeImage == null){
+            throw new RuntimeException("This product has not Barcode image with Id: "+productId);
+        }
+        return barcodeImage;
+    }
+    @Transactional
+    public byte[] getQrCodeImage(Long productId){
+        Product product = productRepo.findById(productId)
+                .orElseThrow(()-> new ProductNotFoundEx("Product not found with id: "+productId));
+
+        byte[] qrCodeImage = product.getQrCodeImage();
+
+        if(qrCodeImage == null){
+            throw new RuntimeException("This product has not QR Code image with Id: "+productId);
+        }
+        return qrCodeImage;
     }
 }
